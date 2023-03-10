2836ee8a3238c9c1212ab8c5bc11e37f3bd39494
itsDipsy <paregos55.collins@gmail.com>
2023-03-10 10:24:59 +0100

    uppdate

---
+++ 13.6.1/problem(3)/problem/README.txt
@@ -0,0 +1,18 @@
+Instruktioner:
+
+1. I denna övning har ni en färdig webbplats (HTML, CSS, en bild) som ni ska utgå ifrån.
+2. Er uppgift är att göra så att det inte finns någon HTML utanför PHP-taggarna, dvs. <?php ... ?>.
+3. Detta innebär att ni måste på något vis skriva ut (echo) all HTML. Detta kan lösas på flera sätt.
+4. Gör detta genom att skapa PHP-filen "index.php" och klistra sedan in all HTML.
+5. Slutresultatet ska se ut på någotvis som exemplet nedan (med lösningen borttagen).
+
+Slutresultat:
+
+<?php
+
+// All er kod...
+
+echo $html;
+?>
+
+- Inga HTML-element får finnas utanför PHPs start- och sluttagg. -
---
+++ 13.6.1/problem(3)/problem/index.php
@@ -0,0 +1,33 @@
+
+<?php 
+    $head = <head>
+                <title>Övning 01</title>
+                <meta charset="utf-8">
+                <link rel="stylesheet" href="styles.css">
+            </head>
+
+?>
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Övning 01</title>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="styles.css">
+        </head>
+        <body>
+            <h1>Breed Profile: An Essential Guide to Corgis (Pembroke Welsh and Cardigan Welsh)</h1>
+            <p>You don’t have to be a corgi lover to recognize one. Their short legs, long back, and fox-like looks make them distinctive (and adorable) in appearance, and their friendly, lively personalities leave a lasting impression on anyone they meet. Learn more about these cheerful herding dogs in this in-depth breed profile, and let us help you decide if a corgi dog is right for you.</p>
+            <h2>Breed Characteristics</h2>
+            <ul>
+                <li>Origin: Wales</li>
+                <li>Size: small</li>
+                <li>Average Life Expectancy: 12-13 years (Pembroke), 12-15 years (Cardigan)</li>
+                <li>Energy Level: medium</li>
+                <li>Breed Group: herding</li>
+            </ul>
+            <img src="corgi.jpg" alt="A Corgi!">
+            <h2>Breed Appearance</h2>
+            <p>The corgi dog type actually consists of two separate breeds: the Pembroke Welsh corgi and the Cardigan Welsh corgi. You may have also heard of cowboy corgis, which aren’t purebred corgis but rather a mix of corgi and Australian cattle dog.</p>
+        </body>
+</html>
